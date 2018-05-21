6f9c20b162046cf716df026f18bcada7e5a4f700
Alexsander Haas <alexsander.haas@hotmail.com>
2018-05-20 21:27:50 -0300

    Commit: Esta salvando os dados no Hbase. 	Tratando os dados recebidos em Json com o Gson, e salvando no Hbase. Removido todos comentarios e métodos antigos. Problema: Ao salvar no Hbase esta tentando inserir mais de uma vez a mesma linha.

--- KafkaHbase/src/main/java/main/ConnectioHbase.java
+++ KafkaHbase/src/main/java/main/ConnectioHbase.java
@@ -1,10 +1,6 @@
 package main;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -18,32 +14,24 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import com.google.protobuf.ServiceException;
 
-import scala.Tuple2;
-
 public class ConnectioHbase {
 
-	// TableName test = TableName.valueOf("test");
+	TableName gv_brolog = TableName.valueOf("BroLog");
 
-	TableName test = TableName.valueOf("kafka");
+	String gv_family_dns = "dns";
 
-	String fam = "cf";
+	Configuration gv_config = HBaseConfiguration.create();
 
-	byte[] column1 = Bytes.toBytes("test1");
+	String gv_path = this.getClass().getClassLoader().getResource("hbase-site.xml").getPath();
 
-	Configuration config = HBaseConfiguration.create();
+	Connection gv_connection;
 
-	String path = this.getClass().getClassLoader().getResource("hbase-site.xml").getPath();
+	Admin gv_admin;
 
-	Connection connection;
-
-	Admin admin;
-
-	Table table;
+	Table gv_table;
 
 	public ConnectioHbase() throws IOException, ServiceException {
 
@@ -53,214 +41,65 @@ public class ConnectioHbase {
 
 	public void Conecta() throws IOException, ServiceException {
 
-		config.addResource(new Path(path));
+		gv_config.addResource(new Path(gv_path));
 
-		connection = ConnectionFactory.createConnection(config);
+		gv_connection = ConnectionFactory.createConnection(gv_config);
 
-		HBaseAdmin.checkHBaseAvailable(config);
+		HBaseAdmin.checkHBaseAvailable(gv_config);
 
-		admin = connection.getAdmin();
+		gv_admin = gv_connection.getAdmin();
 
-		table = connection.getTable(test);
+		gv_table = gv_connection.getTable(gv_brolog);
 
-	}
-
-	public void PutTable(String nome, int n_row) throws IOException, ServiceException {
-
-		// Inserir linhas na tabela
-		// Row1 => Family1:Qualifier1, Family1:Qualifier2
-
-		String rw;
-
-		rw = "row" + getDateTime() + "_" + n_row;
-
-		System.out.println("Linha PUT:" + rw);
-
-		byte[] row_1 = Bytes.toBytes(rw);
-
-		Put p = new Put(row_1);
-
-		p.addImmutable(fam.getBytes(), column1, Bytes.toBytes(nome));
-
-		table.put(p);
-
-		// admin.disableTable(test);
-		try {
-			HColumnDescriptor desc1 = new HColumnDescriptor(row_1);
-			admin.addColumn(test, desc1);
-			System.out.println("Success.");
-		} catch (Exception e) {
-			System.out.println("Failed.");
-			System.out.println(e.getMessage());
-		} finally {
-			// admin.enableTable(test);
-		}
-		// System.out.println("Done. ");
-		// FImInserir linhas na tabela
-
-	}
-
-	public void PutTuple(Tuple2<String, Integer> nome, int n_row) throws IOException, ServiceException {
-
-		// Inserir linhas na tabela
-		// Row1 => Family1:Qualifier1, Family1:Qualifier2
-
-		String rw;
-		byte[] word = Bytes.toBytes("word");
-		byte[] count = Bytes.toBytes("count");
-
-		String val = Integer.toString(nome._2());
-
-		byte[] value1 = Bytes.toBytes(val);
-
-		rw = "row" + getDateTime() + "_" + n_row;
-
-		System.out.println("Count PUT:" + nome._2());
-
-		byte[] row_1 = Bytes.toBytes(rw);
-
-		Put p = new Put(row_1);
-
-		p.addImmutable(fam.getBytes(), word, Bytes.toBytes(nome._1));
-		p.addImmutable(fam.getBytes(), count, value1);
-
-		table.put(p);
-
-		// admin.disableTable(test);
-		try {
-			HColumnDescriptor desc1 = new HColumnDescriptor(row_1);
-			admin.addColumn(test, desc1);
-			System.out.println("Success.");
-		} catch (Exception e) {
-			System.out.println("Failed.");
-			System.out.println(e.getMessage());
-		} finally {
-			// admin.enableTable(test);
-		}
-		// System.out.println("Done. ");
-		// FImInserir linhas na tabela
-
-	}
-
-	private String getDateTime() {
-		DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyyHHmmss");
-		Date date = new Date();
-		return dateFormat.format(date);
 	}
 
 	public void Close() throws IOException {
-		connection.close();
+		gv_connection.close();
 	}
 
-	// feito 11-05
+	public void M_DnsPutTable(Cl_BroDns lo_dns) throws IOException, ServiceException {
 
-	public void M_PutTable(String im_line) throws IOException, ServiceException {
+		byte[] lv_row;		
 
-		// Inserir linhas na tabela
-		// Row1 => Family1:Qualifier1, Family1:Qualifier2
+		lv_row = Bytes.toBytes(lo_dns.getTs());
 
-		String lv_val;
-		String lv_key;
-
-		byte[] lv_row;
-		byte[] lv_coluna;
-		byte[] lv_value;
-
-		JSONObject lv_obj = new JSONObject(im_line);
-		
-		System.out.println("Line: " + im_line);
-		System.out.println("Objeto: " + lv_obj);
-		Iterator<?> keys = lv_obj.keys();
-
-		Object lv_dados;
-		
-		Class cls;
-
-		lv_key = "ts";
-
-		//lv_val = (String) lv_obj.get(lv_key);// Da erro pois vem como booleano e não conveeerte pra string. O consumo
-												// pego do meio de outrrrrrra
-		
-		//lv_row = Bytes.toBytes("23232.455"); inseriu com ponto
-		//lv_row = Bytes.toBytes((Double) lv_obj.get(lv_key)); AJUSTAR PARA SALVAR A LINHA CORRETA
-		
-		lv_dados = lv_obj.get(lv_key);
-		lv_row = lv_dados.toString().getBytes(); // salvou mas ver para salvar o mesmo valor que recebe pois esta indo com EA
-		
 		Put ls_table = new Put(lv_row);
+
+		// ----------- Uid
+		M_PutLs(lo_dns.gs_colunas[1], lo_dns.getUid(), ls_table);
+
+		// ----------- Id.Orig_H
+		M_PutLs(lo_dns.gs_colunas[2], lo_dns.getOrig_h(), ls_table);
+
+
+		// ----------- Id.Orig_P
+		M_PutLs(lo_dns.gs_colunas[3], lo_dns.getOrig_p(), ls_table);
+
+
+		// ----------- Id.Resp_H
+		M_PutLs(lo_dns.gs_colunas[4], lo_dns.getResp_h(), ls_table);
+	
+
+		// ----------- Id.Orig_P
+		M_PutLs(lo_dns.gs_colunas[5], lo_dns.getResp_p(), ls_table);
 		
-		System.out.println("Row: " + lv_obj.get(lv_key) + "   byte" + lv_row);
-		//while (keys.hasNext()) {
 
-			//lv_key = (String) keys.next();
-			
-			System.out.println("Key: " + lv_key);
-			System.out.println("Value: " + lv_obj.get(lv_key));
-			
-			
-			
-			lv_coluna = Bytes.toBytes(lv_key);
+		// ----------- Proto
+		M_PutLs(lo_dns.gs_colunas[6], lo_dns.getProto(), ls_table);
+		
 
-			lv_dados = lv_obj.get(lv_key);
-			
-			cls = lv_dados.getClass();
-						
-			System.out.println("col: " + lv_key + "byte" + lv_coluna);
+		// ----------- Query
+		M_PutLs(lo_dns.gs_colunas[7], lo_dns.getQuery(), ls_table);
 
-			/*if (lv_dados instanceof Boolean) {
+		// ----------- PutTable
 
-				lv_value = Bytes.toBytes((Boolean)lv_dados);
+		System.out.println("LS_TABLE: " + ls_table);
 
-			}else if(lv_dados instanceof JSONArray)  {
-				//lv_value = Bytes.toBytes((JSONArray) lv_dados);
-			}*/
+		gv_table.put(ls_table);
 
-			//lv_value = Bytes.toBytes((String) lv_obj.get(lv_key)); // Da erro pois vem como booleano e não converte
-																	// pra string
-			//if (lv_dados instanceof String) {
-
-				//lv_value = Bytes.toBytes("TESTERR");//((String)lv_dados);
-				
-				lv_value = lv_dados.toString().getBytes();
-				
-				System.out.println("value: " + lv_dados + "byte" + lv_value);
-				
-				ls_table.addImmutable(fam.getBytes(), lv_coluna, lv_value);
-				System.out.println("LS_TABLE: " + ls_table);
-				
-			//}					
-
-		//}
-
-		table.put(ls_table);
-
-		/*
-		 * String rw; byte[] word = Bytes.toBytes("word"); byte[] count =
-		 * Bytes.toBytes("count");
-		 * 
-		 * String val = Integer.toString(nome._2());
-		 * 
-		 * byte[] value1 = Bytes.toBytes(val);
-		 * 
-		 * rw = "row" + getDateTime() + "_" + n_row;
-		 * 
-		 * System.out.println("Count PUT:" + nome._2());
-		 * 
-		 * byte[] row_1 = Bytes.toBytes(rw);
-		 * 
-		 * Put p = new Put(row_1);
-		 * 
-		 * p.addImmutable(fam.getBytes(), word, Bytes.toBytes(nome._1));
-		 * p.addImmutable(fam.getBytes(), count, value1);
-		 * 
-		 * 
-		 * table.put(p);
-		 */
-
-		// admin.disableTable(test);
 		try {
 			HColumnDescriptor desc1 = new HColumnDescriptor(lv_row);
-			admin.addColumn(test, desc1);
+			gv_admin.addColumn(gv_brolog, desc1);
 			System.out.println("Success.");
 		} catch (Exception e) {
 			System.out.println("Failed.");
@@ -268,10 +107,25 @@ public class ConnectioHbase {
 		} finally {
 			// admin.enableTable(test);
 		}
-		// System.out.println("Done. ");
-		// FImInserir linhas na tabela
+
+	}
+
+	private Put M_PutLs(String col, String value, Put ls_table) {
+
+		byte[] lv_coluna;
+		byte[] lv_value;
+
+		if (value != null) {
+
+			lv_coluna = Bytes.toBytes(col);
+
+			lv_value = Bytes.toBytes(value);
+
+			ls_table.addImmutable(gv_family_dns.getBytes(), lv_coluna, lv_value);
+		}
+
+		return ls_table;
 
 	}
 
 }
-
--- KafkaHbase/src/main/java/main/Kafka_Hbase.java
+++ KafkaHbase/src/main/java/main/Kafka_Hbase.java
@@ -3,26 +3,24 @@ package main;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
@@ -30,24 +28,19 @@ import org.apache.spark.streaming.kafka010.LocationStrategies;
 
 import com.google.protobuf.ServiceException;
 
-import scala.Tuple2;
-import scala.util.parsing.json.JSONArray;
-
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import main.ConnectioHbase;
-import org.json.JSONObject;
 
 public class Kafka_Hbase {
 
 	private static Kafka_Hbase go_kafka;
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		// new Kafka_Hbase().Consome();
 
 		go_kafka = new Kafka_Hbase();
 
@@ -92,146 +85,46 @@ public class Kafka_Hbase {
 				return kafkaRecord.value();
 			}
 		});
-
-		// Break every message into words and return list of words
-		JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-			public Iterator<String> call(String line) throws Exception {
-				
-				System.out.println("LINES: " + line);				
-				
-				ConnectioHbase lo_hbase = new ConnectioHbase();
-				
-				
-				
-				/*JSONObject lv_obj = new JSONObject(line);
-
-				Iterator<?> keys = lv_obj.keys();
-				String key = (String) keys.next();*/
-				//System.out.println("Key: " + key);
-				//System.out.println("Value: " + lv_obj.get("ts"));
-				/*while (keys.hasNext()) {
-					String key = (String) keys.next();
-					System.out.println("Key: " + key);
-					System.out.println("Value: " + lv_obj.get(key));
-				}*/
-				lo_hbase.M_PutTable(line);
-
-				return Arrays.asList(line.split(" ")).iterator();
-			}
-		});
-
-		// Take every word and return Tuple with (word,1)
-		JavaPairDStream<String, Integer> wordMap = words.mapToPair(new PairFunction<String, String, Integer>() {
-			public Tuple2<String, Integer> call(String word) throws Exception {
-				// go_kafka.ReadTable(word);
-				// go_kafka.PutTable(word);
-				String[] colunas = word.split(" ");
-
-				// Tennnnntar utttilizar aaaaaaaaaaaaas classse json pra pegar aas
-				// informaaaaaaaações
-				// JSONArray arr = new JSONArray(colunas);
-
-				/*System.out.println("Key: " + word);
-
-				JSONObject lv_obj = new JSONObject(word);
-
-				Iterator<?> keys = lv_obj.keys();
-
-				while (keys.hasNext()) {
-					String key = (String) keys.next();
-					System.out.println("Key: " + key);
-					System.out.println("Value: " + lv_obj.get(key));
-				}*/
-
-				/*
-				 * System.out.println("Linha Kafka:"+ lv_obj); if (colunas.length > 7) { for
-				 * (int i = 0; i < 8; i++) { System.out.println(i + "- Coluna Kafka:" +
-				 * colunas[i]); } }
-				 */
-
-				// System.out.println("Coluna Kafka:" + word);
-				return new Tuple2<String, Integer>(word, 1);
-			}
-		});
-
-		// Count occurance of each word
-		JavaPairDStream<String, Integer> wordCount = wordMap.reduceByKey(new Function2<Integer, Integer, Integer>() {
-			public Integer call(Integer first, Integer second) throws Exception {
-				return first + second;
-			}
-		});
-
 		
-
-		// Print the word count
-		wordCount.print();
-
+		lines.foreachRDD(rdd -> {
+			
+			System.out.println("Dados:Do RDDe" + rdd.count());
+			
+ 			rdd.foreachPartition(partitionOfRecords -> {							
+ 				
+ 				String value;
+ 				
+ 				ConnectioHbase lo_connection = new ConnectioHbase(); 			
+ 				
+ 			// Get Gson object
+ 				Gson gson = new GsonBuilder().setPrettyPrinting().create();
+ 				
+ 			// parse json string to object
+ 				Cl_BroDns lo_dns;// = gson.fromJson(fileData, Employee.class);
+ 				
+ 				while (partitionOfRecords.hasNext()) {
+ 					
+ 					value = partitionOfRecords.next();
+ 					
+ 					lo_dns = gson.fromJson(value, Cl_BroDns.class);
+ 		
+ 					lo_connection.M_DnsPutTable(lo_dns);
+ 					
+ 				}						
+ 				
+ 				System.out.println("Dados:Apos o while");
+ 				
+ 				lo_connection.Close();
+ 				
+ 			});
+ 
+ 		});
+		
 		jssc.start();
 		jssc.awaitTermination();
 
 	}
-
-	public void PutTable(String nome) throws IOException, ServiceException {
-
-		TableName test = TableName.valueOf("test");
-
-		String fam = "dns";
-
-		byte[] column1 = Bytes.toBytes("a");
-
-		Configuration config = HBaseConfiguration.create();
-
-		String path = this.getClass().getClassLoader().getResource("hbase-site.xml").getPath();
-
-		config.addResource(new Path(path));
-
-		Connection connection = ConnectionFactory.createConnection(config);
-
-		HBaseAdmin.checkHBaseAvailable(config);
-
-		Admin admin = connection.getAdmin();
-
-		Table table = connection.getTable(test);
-
-		// Inserir linhas na tabela
-		// Row1 => Family1:Qualifier1, Family1:Qualifier2
-
-		String rw;
-
-		rw = "row" + getDateTime();
-
-		System.out.println("Linha PUT:" + rw);
-
-		byte[] row_1 = Bytes.toBytes(rw);
-
-		Put p = new Put(row_1);
-
-		p.addImmutable(fam.getBytes(), column1, Bytes.toBytes(nome));
-
-		table.put(p);
-
-		// admin.disableTable(test);
-		try {
-			HColumnDescriptor desc1 = new HColumnDescriptor(row_1);
-			admin.addColumn(test, desc1);
-			System.out.println("Success.");
-		} catch (Exception e) {
-			System.out.println("Failed.");
-			System.out.println(e.getMessage());
-		} finally {
-			// admin.enableTable(test);
-		}
-		// System.out.println("Done. ");
-		// FImInserir linhas na tabela
-
-	}
-
-	private String getDateTime() {
-		DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyyHHmmss");
-		Date date = new Date();
-		return dateFormat.format(date);
-	}
-
+	
 	public void ReadTable(String nome) throws IOException, ServiceException {
 
 		TableName test = TableName.valueOf("test");
